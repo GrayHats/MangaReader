2f9e75224ff6ff7504e2ef57d987070766ab565f
allanon <allanon@e2d3a20a-4431-42a0-8629-dbdcde93018f>
2011-06-10 19:00:48 +0000

    git-svn-id: http://community.grayhats.org/public/mangareader/trunks@15 e2d3a20a-4431-42a0-8629-dbdcde93018f

--- mangareader.py
+++ mangareader.py
@@ -7,8 +7,24 @@ import tarfile
 import shutil
 from datetime import datetime
 
+'''
+work in progress, obiettivi
+
+- eliminare beautiful soup
+- compilare tutte le regen
+- dare na sistemata in generale
+- commentare il codice
+
+'''
+
+
+
+
+
+stdout=1 # debug value
 def put(string):
-	sys.stdout.write(string + '\n')
+	if stdout:
+		sys.stdout.write(string + '\n')
 
 def put_err(string):
 	sys.stderr.write(string + '\n')
@@ -56,37 +72,54 @@ def download_img(links, title):
 
 def download_chapter(url_chapter):
 	x = mangareader(url_chapter)
-	manga = x.fetch_title()
-	name = x.convert_name(manga) # converte il nome del manga in formato utile per mangareader
-	title = x.convert_name(str(x.fetch_links('%s \d\d*' % (manga), 'title')[0]))
-	#<option value="/fairy-tail/216/5">5</option>
-	pages = []
-	pages = x.fetch_links('/\d*-\d*-\d*/%s/.*html' %(name), 'option',1)	
-	if len(pages) == 0:
-		pages = x.fetch_links('/%s/\d\d*/\d\d*' %(name), 'option',1)	
-	pages.insert(0,url_chapter)
-	links=[]
-	for page in pages:
-		z = mangareader(str(page))
-		for link in z.fetch_links('http://.*%s.*jpg' %(name),'src'):
-			links.append(link)
-	download_img(links, title)
+	#manga = x.fetch_title_manga()
+	#name = x.convert_name(manga) # converte il nome del manga in formato utile per mangareader
+	title = x.convert_name(x.fetch_title_chapter())
+	pages = x.fetch_links_chapter()
+	imgs=[]
+	imgs.append(x.fetch_link_img())
+	for page in pages[1:]: # evitiamo di riscaricare la prima pagina, visto che gia' l'abbiamo
+		z = mangareader(page)
+		imgs.append(z.fetch_link_img())
+	download_img(imgs, title)
 	return 1
 
 class mangareader:
 	def __init__(self, url):
-		''' fetch the page and parse with beatifulsoup'''
+		''' 
+		scarica la pagina e compila le regex
+		url - stringa che contiene l'indirizzo
+		'''
 		try:
-			page = urllib2.urlopen(url)
+			usock = urllib.urlopen(url)
+			self.page=usock.read()
+			usock.close()
 		except:
 			print "pagina non scaricata: %s" %(url, ) 
 			sys.exit(1)
-		self.soup = BeautifulSoup(page)	
-		#self.manga = self_title
+		# compiled regex
+		self.rtitlemanga = re.compile('(?<=<title>).*?(?= Manga)')
+		self.rtitlechapter = re.compile('(?<=<title>).*?(?= \d+ - Read)')
+		self.rimg = re.compile('(?<=src=")http://.*jpg(?=")')
+		self.roption = re.compile('(?<=option value=").*?(?=")')
+
+	def fetch_tag(self, regex):
+		'''
+		semplice ricerca per regex
+		ritorna 0, stringa o lista
+		'''
+		rows = regex.findall(self.page)
+		if len(rows) == 0:
+			return 0
+		if len(rows) == 1:
+			return rows[0] # regex per titoli e img dovrebbero sempre tornare 1 solo risultato, se non cambiano le cose
+		return rows # regex per options e links..
+
+
 
 	def looking_for(self, manga):
 		'''example: looking for Fairy Tail release'''
-		trovato = self.soup.findAll(text=re.compile('%s \d*' % (manga,)))
+		trovato = self.page.findall(text=re.compile('%s \d*' % (manga,)))
 		if trovato:
 			return trovato
 		else:
@@ -95,21 +128,46 @@ class mangareader:
 	def convert_name(self, manga):
 		return manga.replace("'","").replace(' - ',' ').replace(' ','-').lower()
 
-	def fetch_title(self):
-		raw_title = self.soup.title.contents[0]
-		p1 = re.search('.*(?= Manga)',raw_title)
-		if not p1:
-			p2=re.search('.*(?= \d+ - Read)', raw_title)
-			return p2.group(0)
-		return p1.group(0)
-		##p4=re.compile('\d+$')   #appunto per trovare il numero di capitolo
+	def fetch_title_manga(self):
+		'''
+		Ritorna il titolo del Manga
 
+		return string
+		'''
+		return self.fetch_tag(self.rtitlemanga)
+
+	def fetch_title_chapter(self):
+		'''
+		Ritorna il titolo del capitolo
+		solitamente e' nella forma 
+		NomeManga NumeroCapitolo
+
+		return string
+		'''
+		return self.fetch_tag(self.rtitlechapter)
+	
+	def fetch_links_chapter(self):
+		'''
+		ritorna i link delle pagine collegate al capitolo
+
+		return list
+		'''
+		links=[]
+		for row in self.fetch_tag(self.roption):
+			links.append(self.build_name(row))
+		return links
+
+	def fetch_link_img(self):
+		return self.fetch_tag(self.rimg)
+
+	# old, da cancellare
 	def fetch_chapter(self):
 		raw_title = self.soup.title.contents[0]
 		p2=re.search('.*(?= - Read)', raw_title)
 		return p2.group(0)
 		##p4=re.compile('\d+$')   #appunto per trovare il numero di capitolo
 
+	# old, da cancellare
 	def fetch_links(self, regexpr, attr='href', convert=0):
 		'''Fetch links for a centain manga '''
 		## looking for links 
@@ -132,5 +190,12 @@ class mangareader:
 		return links
 
 	def build_name (self, link):
+		'''
+		ricostruisce il link assoluto a partire da un link relativo alla pagina mangareader.net
+
+		link - string
+		return string
+		'''
+
 		return 'http://www.mangareader.net%s' % (str(link),)
 
