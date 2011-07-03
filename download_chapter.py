38e3200d3ee9e92ad62ea4440dbc3932aaeb628a
root <root@tenseiga>
2011-07-03 12:56:27 +0200

    pylintvalue 10!

--- download_chapter.py
+++ download_chapter.py
@@ -3,17 +3,15 @@
 scarica manga
 '''
 
-from mangareader import mangareader
 from mangareader import download_chapter
-from mangareader import download_img
 import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 1 :
-        url = raw_input('Inserisci l\'url ... ')
+        URL = raw_input('Inserisci l\'url ... ')
     else:
-        url = sys.argv[1]
-    download_chapter(url)
+        URL = sys.argv[1]
+    download_chapter(URL)
             
 
 
