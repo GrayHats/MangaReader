95000b4d6722824a4eef33d455e39b79097b3c2c
allanozzolo <allanon@alaimo.org>
2011-10-01 06:56:54 +0200

    correzioni minori

--- mangareader.py
+++ mangareader.py
@@ -21,13 +21,13 @@ def stampa_err(string):
 
 class logfile:
     def __init__(self, logfile):
-    	self.logfile = open(logfile, 'a')
+        self.logfile = open(logfile, 'a')
 
     def logga(self, string):
-    	self.logfile.write(string + '\n')
+        self.logfile.write(string + '\n')
 
     def close(self):
-    	self.logfile.close()
+        self.logfile.close()
 
 def download_img(links, title):
     import tarfile
@@ -37,39 +37,39 @@ def download_img(links, title):
     directory = os.path.join(dirmanga,title)
     #retcode = subprocess.call([dirname+'/checkdir.sh', directory])
     #if not os.path.isdir(dirmanga):
-    #	os.mkdir(dirmanga)
+    #    os.mkdir(dirmanga)
     try:
-    	os.makedirs(directory)
+        os.makedirs(directory)
     except:
-    	from datetime import datetime
-    	now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    	os.rename(directory, directory+now)
-    	os.makedirs(directory)
+        from datetime import datetime
+        now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        os.rename(directory, directory+now)
+        os.makedirs(directory)
     os.chdir(dirmanga)
     for link in links:
-    	file_manga = link[link.rfind('/')+1:]
-    	for i in range(1,10):
-    		try:
-    			urllib.urlretrieve(str(link), os.path.join(directory, file_manga))	
-    			stampa('  -> scaricato: %s '% (str(link),))
-    			break
-    		except:
-    			from time import sleep
-    			stampa_err('  -> ERRORE nello scaricare: %s\nRiprovo'% (str(link),))
-    			sleep(1)
-    			continue
-    		stampa_err('  -> ERRORE nello scaricare: %s\nEsco.'% (str(link),))
-    		sys.exit(-1)
+        file_manga = link[link.rfind('/')+1:]
+        for i in range(1,10):
+            try:
+                urllib.urlretrieve(str(link), os.path.join(directory, file_manga))    
+                stampa('  -> scaricato: %s '% (str(link),))
+                break
+            except:
+                from time import sleep
+                stampa_err('  -> ERRORE nello scaricare: %s\nRiprovo'% (str(link),))
+                sleep(1)
+                continue
+            stampa_err('  -> ERRORE nello scaricare: %s\nEsco.'% (str(link),))
+            sys.exit(-1)
 
     try:
-    	tar = tarfile.open(title+'.cbz', 'w')
-    	tar.add(title)
-    	tar.close()
-    	stampa('  Creato %s\n' %(title+'.cbz',))
-    	shutil.rmtree(title)
+        tar = tarfile.open(title+'.cbz', 'w')
+        tar.add(title)
+        tar.close()
+        stampa('  Creato %s\n' %(title+'.cbz',))
+        shutil.rmtree(title)
     except:
-    	os.chdir(dirname)
-    	sys.exit(-1)
+        os.chdir(dirname)
+        sys.exit(-1)
     os.chdir(dirname)
     return 1
 
@@ -82,114 +82,114 @@ def download_chapter(url_chapter):
     imgs=[]
     imgs.append(x.fetch_link_img())
     for page in pages[1:]: # evitiamo di riscaricare la prima pagina, visto che gia' l'abbiamo
-    	z = mangareader(page)
-    	imgs.append(z.fetch_link_img())
+        z = mangareader(page)
+        imgs.append(z.fetch_link_img())
     stampa(' -> Trovate %s pagine e %s immagini' % (len(pages),len(imgs)))
     if len(pages) != len(imgs):
-    	stampa_err('ERRORE: numero di pagine e immagini differente nel capitolo: %s' %(url_chapter,))
-    	sys.exit(-1)
+        stampa_err('ERRORE: numero di pagine e immagini differente nel capitolo: %s' %(url_chapter,))
+        sys.exit(-1)
     #for img in imgs:
-    #	stampa('   -> %s' % img)
+    #    stampa('   -> %s' % img)
     if download_img(imgs, title):
-    	return 1
+        return 1
     sys.exit(-1)
 
 class mangareader:
-	def __init__(self, url):
-		''' 
-		scarica la pagina e compila le regex
-		url - stringa che contiene l'indirizzo
-		'''
-		try:
-			usock = urllib.urlopen(url)
-			self.page=usock.read()
-			usock.close()
-		except:
-			print "pagina non scaricata: %s" %(url, ) 
-			sys.exit(1)
-		# compiled regex
-		self.rtitlemanga = re.compile('(?<=<title>).*?(?= Manga)')
-		self.rtitlechapter = re.compile('(?<=<title>).*?(?= - Read)')
-		self.rimg = re.compile('(?<=src=")http://.*jpg(?=")')
-		self.roption = re.compile('(?<=option value=").*?(?=")')
+    def __init__(self, url):
+        ''' 
+        scarica la pagina e compila le regex
+        url - stringa che contiene l'indirizzo
+        '''
+        try:
+            usock = urllib.urlopen(url)
+            self.page=usock.read()
+            usock.close()
+        except:
+            print "pagina non scaricata: %s" %(url, ) 
+            sys.exit(1)
+        # compiled regex
+        self.rtitlemanga = re.compile('(?<=<title>).*?(?= Manga)')
+        self.rtitlechapter = re.compile('(?<=<title>).*?(?= - Read)')
+        self.rimg = re.compile('(?<=src=")http://.*jpg(?=")')
+        self.roption = re.compile('(?<=option value=").*?(?=")')
 
-	def fetch_tag(self, regex):
-		'''
-		semplice ricerca per regex
-		ritorna 0 oppure lista
-		'''
-		rows = regex.findall(self.page)
-		#if len(rows) == 0:
-			#return 0
-		return rows 
-
-
-	def convert_name(self, manga):
-		return manga.replace("'","").replace(' - ',' ').replace(' ','-').replace('!','').replace(':','').lower()
-
-	def fetch_title_manga(self):
-		'''
-		Ritorna il titolo del Manga
-
-		return string
-		'''
-		return self.fetch_tag(self.rtitlemanga)[0]
-
-	def fetch_title_chapter(self):
-		'''
-		Ritorna il titolo del capitolo
-		solitamente e' nella forma 
-		NomeManga NumeroCapitolo
-
-		return string
-		'''
-		return self.fetch_tag(self.rtitlechapter)[0]
-	
-	def fetch_pages_chapter(self):
-		'''
-		ritorna i link delle pagine collegate al capitolo
-
-		return list
-		'''
-		links=[]
-		for row in self.fetch_tag(self.roption):
-			links.append(self.build_name(row))
-		return links
-
-	def fetch_link_img(self):
-		return self.fetch_tag(self.rimg)[0]
-
-	def fetch_chapters_manga(self, nomemanga):
-		'''
-		ritorna i link dei capitoli collegate ad un manga
-
-		return 0, list
-		'''
-		regex1 = re.compile('(?<=href=")/' + nomemanga + '.*?(?=")')
-		regex2 = re.compile('(?<=href=")/\d*-\d*-\d*/' + nomemanga + '/chapter-\d*.html' + '(?=")')
-		links=[]
-		rows1 = self.fetch_tag(regex1)
-		rows2 = self.fetch_tag(regex2)
-		if len(rows1) != 0 :
-			for row in rows1:
-				if self.build_name(row) not in links:
-					links.append(self.build_name(row))
-		if len(rows2) != 0 :
-			for row in rows2:
-				if self.build_name(row) not in links:
-					links.append(self.build_name(row))
-		if len(links) == 0:
-			return 0
-		return links
+    def fetch_tag(self, regex):
+        '''
+        semplice ricerca per regex
+        ritorna 0 oppure lista
+        '''
+        rows = regex.findall(self.page)
+        #if len(rows) == 0:
+            #return 0
+        return rows 
 
 
+    def convert_name(self, manga):
+        return manga.replace("'","").replace(' - ',' ').replace(' ','-').replace('!','').replace(':','').lower()
 
-	def build_name (self, link):
-		'''
-		ricostruisce il link assoluto a partire da un link relativo alla pagina mangareader.net
+    def fetch_title_manga(self):
+        '''
+        Ritorna il titolo del Manga
 
-		link - string
-		return string
-		'''
+        return string
+        '''
+        return self.fetch_tag(self.rtitlemanga)[0]
 
-		return 'http://www.mangareader.net%s' % (str(link),)
+    def fetch_title_chapter(self):
+        '''
+        Ritorna il titolo del capitolo
+        solitamente e' nella forma 
+        NomeManga NumeroCapitolo
+
+        return string
+        '''
+        return self.fetch_tag(self.rtitlechapter)[0]
+    
+    def fetch_pages_chapter(self):
+        '''
+        ritorna i link delle pagine collegate al capitolo
+
+        return list
+        '''
+        links=[]
+        for row in self.fetch_tag(self.roption):
+            links.append(self.build_name(row))
+        return links
+
+    def fetch_link_img(self):
+        return self.fetch_tag(self.rimg)[0]
+
+    def fetch_chapters_manga(self, nomemanga):
+        '''
+        ritorna i link dei capitoli collegate ad un manga
+
+        return 0, list
+        '''
+        regex1 = re.compile('(?<=href=")/' + nomemanga + '.*?(?=")')
+        regex2 = re.compile('(?<=href=")/\d*-\d*-\d*/' + nomemanga + '/chapter-\d*.html' + '(?=")')
+        links=[]
+        rows1 = self.fetch_tag(regex1)
+        rows2 = self.fetch_tag(regex2)
+        if len(rows1) != 0 :
+            for row in rows1:
+                if self.build_name(row) not in links:
+                    links.append(self.build_name(row))
+        if len(rows2) != 0 :
+            for row in rows2:
+                if self.build_name(row) not in links:
+                    links.append(self.build_name(row))
+        if len(links) == 0:
+            return 0
+        return links
+
+
+
+    def build_name (self, link):
+        '''
+        ricostruisce il link assoluto a partire da un link relativo alla pagina mangareader.net
+
+        link - string
+        return string
+        '''
+
+        return 'http://www.mangareader.net%s' % (str(link),)
