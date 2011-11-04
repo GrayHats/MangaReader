dec97bca5a95283118cbd396772029e631667755
allanozzolo <allanon@alaimo.org>
2011-11-04 19:32:27 +0100

    funziona

--- fetch_news.py
+++ fetch_news.py
@@ -5,6 +5,11 @@ from mangareader import stampa
 from mangareader import download_chapter
 from mangareader import store, Mail, Manga, Chapter
 #from mangareader import stampa
+from datetime import datetime
+
+
+def now():
+    return datetime.now()
 
 def send_mail(body):
     import smtplib
@@ -31,32 +36,40 @@ if __name__ == "__main__":
                 links.append(row)
     # check links against db
     for link in links:
-        link_t = (link,)
-        if not store.find(Chapter.link, Chapter == unicode(link)).count():
+        #link_t = (link,)
+        if not store.find(Chapter.link, Chapter.link == unicode(link)).count():
+            print 'aggiungo %s' % link
             new_chapter = Chapter()
             new_chapter.link = unicode(link)
             new_chapter.status = 0
+            store.add(new_chapter)
+    store.commit()
 
     # download links with status 0
-    links = store.find(Chapter.link, Chapter.status == 0)
-    if links.count():
+    rows = store.find(Chapter, Chapter.status == 0)
+    if rows.count():
         stampa('Da Scaricare:')
-        for link in links:
-            stampa(' %s' %link)
-        #for link in links:
-        #    stampa('\nScarico %s' %link)
-        #    if download_chapter(link[0]) :
+        for row in rows:
+            stampa(' %s' % row.link)
+        for row in rows:
+            stampa('\nScarico %s' % row.link)
+            #if download_chapter(row.link) :
+            if 1 == 1 :
+                row.status = 1
+                row.data = now()
+                store.commit()
         #        link_t = (link[0],)
         #        c.execute('''update chapters set status = 1 where link like ?  ''', link_t)
-        #        body += '\nScaricato %s:\n' % (link[0])
-        #    else:
-        #        body += '\nErrore nello scaricare %s:\n' % (link)
+                body += '\nScaricato %s:\n' % (row.link)
+            else:
+                body += '\nErrore nello scaricare %s:\n' % (row.link)
 
     # report links with status 0
-    links = store.find(Chapter.link, Chapter.status == 0)
-    if links.count():
+    rows = store.find(Chapter, Chapter.status == 0)
+    if rows.count():
         body += '\n\n###Da scaricare'
-        for link in links:
-            body += '\nDa scaricare %s:\n' % (link[0])
+        for row in rows:
+            body += '\nDa scaricare %s:\n' % (row.link)
     #if body:
     #    send_mail(body, smtp, fromaddr, toaddr,subject)
+    print body
