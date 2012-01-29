359c838565fd9cb1880b552cc589fad67e1ef52d
allanozzolo <allanon@grayhats.org>
2012-01-29 16:32:45 +0100

    modifica minore

--- renameprova.sh
+++ renameprova.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-find . -name *.cbz -exec rename -n 's/(\w+-)(\d{1})\.cbz/${1}00${2}\.cbz/' {} \;
-find . -name *.cbz -exec rename -n 's/(\w+-)(\d{2})\.cbz/${1}0${2}\.cbz/' {} \;
+find $1 -name *.cbz -exec rename -n 's/(\w+-)(\d{1})\.cbz/${1}00${2}\.cbz/' {} \;
+find $1 -name *.cbz -exec rename -n 's/(\w+-)(\d{2})\.cbz/${1}0${2}\.cbz/' {} \;
 ## Basic rename
 # rename -v 's/(\w+-)(\d{1})\.cbz/${1}00${2}\.cbz/' *.cbz
 # rename -v 's/(\w+-)(\d{2})\.cbz/${1}0${2}\.cbz/' *.cbz
